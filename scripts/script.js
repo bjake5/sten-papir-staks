a09adeca3d2e5765330462d80efd7bfc1bfc8244
Blake <21201344+bjake5@users.noreply.github.com>
2024-07-22 14:04:08 -0400

    Refactor names to improve readability

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,26 @@
 
 // List of player object IDs from HTML page 
-const playerTitle = document.querySelector("#player-title");
-const playerRockButton = document.querySelector("#player-rock-selection");
-const playerPaperButton = document.querySelector("#player-paper-selection");
-const playerScissorsButton = document.querySelector("#player-scissors-selection");
-const playerScoreCopy = document.querySelector("#playerScoreCopy");
-const playerHeadline = document.querySelector("#playerHeadline");
-const playerLastSelection = document.querySelector("#playerLastSelection");
+const playerTitle = document.querySelector("#player");
+const playerRockButton = document.querySelector("#player-actions-rock-selection");
+const playerPaperButton = document.querySelector("#player-actions-paper-selection");
+const playerScissorsButton = document.querySelector("#player-actions-scissors-selection");
+const playerScoreCopy = document.querySelector("#player-content-score-copy");
+const playerHeadline = document.querySelector("#player-content-headline");
+const playerLastSelection = document.querySelector("#player-content-last-selection");
+const playerGameResult = document.querySelector("#player-content-game-result");
 const playerActions = document.querySelector("#player-actions");
 const buttons = document.querySelectorAll("button");
 
 //List of computer object IDs from HTML page
-const computerTitle = document.querySelector("#computer-title");
-const computerHeadline = document.querySelector("#computerHeadline");
-const computerLastSelection = document.querySelector("#computerLastSelection");
-const computerScoreCopy = document.querySelector("#computerScoreCopy");
+const computerTitle = document.querySelector("#computer");
+const computerHeadline = document.querySelector("#computer-content-headline");
+const computerLastSelection = document.querySelector("#computer-content-last-selection");
+const computerScoreCopy = document.querySelector("#computer-content-score-copy");
+const computerGameResult = document.querySelector("#computer-content-game-result");
 const computerActions = document.querySelector("#computer-actions");
 
 // Section for play again button after game is finished
-const playAgainSection = document.querySelector("#playAgain");
+const playAgainSection = document.querySelector("#play-again");
 
 // List of constant variables for copy, colors, etc.
 const defaultFontColor = '#2f4f4f';
@@ -29,6 +31,8 @@ const computerLossHeadline = "Computer lost!"
 const tieHeadline = "You tied!"
 const winFontColor = '#7cb69d';
 const lossFontColor = '#f69697';
+const winEmoji = "\u{1F3C6}";
+const lossEmoji = "\u{1F91D}";
 
 // Temporary variables for gameplay
 let humanChoice;
@@ -47,9 +51,9 @@ buttons.forEach((button) => {
         if(humanScore >= 5 || computerScore >= 5) { // Check if the game is over before starting another round
             return false;
         } else { // Map button IDs to human choice variable for gameplay
-            if(button.id == "player-rock-selection") { humanChoice = "rock" };
-            if(button.id == "player-paper-selection") { humanChoice = "paper" }; 
-            if(button.id == "player-scissors-selection") { humanChoice = "scissors" }; 
+            if(button.id == "player-actions-rock-selection") { humanChoice = "rock" };
+            if(button.id == "player-actions-paper-selection") { humanChoice = "paper" }; 
+            if(button.id == "player-actions-scissors-selection") { humanChoice = "scissors" }; 
 
             let humanSelection = humanChoice; // Map human choice variable into an argument
             let computerSelection = getComputerChoice(); // Get the computer choice for gameplay
@@ -146,17 +150,22 @@ function finishGame() {
         playerTitle.style.backgroundColor = winFontColor;
         playerActions.style.backgroundColor = winFontColor;
         playerLastSelection.style.color = defaultFontColor;
-        playerHeadline.textContent = "\u{1F3C6}";
-        playerHeadline.style.fontSize = "40px";
+        playerHeadline.style.color = defaultFontColor;
+        playerHeadline.textContent = "You won the game!";
+        playerGameResult.textContent = winEmoji;
         computerHeadline.textContent = "Computer lost the game!"
+        computerGameResult.textContent = lossEmoji;
 
     } else {
         computerTitle.style.backgroundColor = winFontColor;
         computerActions.style.backgroundColor = winFontColor;
+        computerHeadline.style.color = defaultFontColor;
         computerLastSelection.style.color = defaultFontColor;
-        computerHeadline.textContent = "\u{1F3C6}";
-        computerHeadline.style.fontSize = "40px";
+        computerHeadline.textContent = "Computer won the game!";
+        computerGameResult.textContent = winEmoji;
         playerHeadline.textContent = "You lost the game!"
+        playerGameResult.textContent = lossEmoji;
+
     }
     playerRockButton.remove();
     playerPaperButton.remove();
@@ -164,7 +173,7 @@ function finishGame() {
     const playAgainButton = document.createElement("button");
     playAgainSection.appendChild(playAgainButton);
     playAgainButton.style.color = defaultFontColor;
-    playAgainButton.textContent = "Play again?";
+    playAgainButton.textContent = "Play Again?";
     
     // Reload page onClick Play again? button
     playAgainButton.addEventListener("click", () => {
